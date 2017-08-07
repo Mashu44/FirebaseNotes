dafffd9d2a2bb9d9ef195222148c03e09837b748
Mashu44 <30735098+Mashu44@users.noreply.github.com>
2017-08-06 22:24:36 -0500

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,170 @@
+
+
+function signUp()
+{
+	
+
+	var email = document.getElementById('txtEmail').value;
+	var password = document.getElementById('txtPassword').value;
+	firebase.auth().createUserWithEmailAndPassword(email,password).then(function(data1){
+		
+
+		var JsonKey = 
+		{
+		"uid": data1.uid,
+		"txtEmail":email
+		}
+		firebase.database().ref('users/' + data1.uid).set(JsonKey).then(function(user){
+			if (user) 
+			{
+				alert("Signed up!")
+
+				window.location.href = "notes.html";
+	       	} 
+        else 
+        {
+        	alert("Account created succesfully!")
+        	window.location.href ="notes.html";   
+
+        	
+        }
+    	}).catch(function(error)
+        {
+        	console.log(error);
+        	alert(error);
+        });
+		
+}).catch(function(error)
+{
+	console.log(error);
+	alert(error);
+});
+}
+
+function signIn()
+{
+	
+	var email = document.getElementById('txtEmail').value;
+	var password = document.getElementById('txtPassword').value;
+	firebase.auth().signInWithEmailAndPassword(email,password).then(function(user){
+			if (user) 
+		{
+        	document.location.href ="notes.html";
+        } 
+        else 
+        {
+
+        }
+    }).catch(function(error)
+        {
+        	console.log(error);
+        	alert(error);
+        });
+		
+	
+	
+}
+//function logInState()
+//{
+//
+//	firebase.auth().onAuthStateChanged(function(user) {
+  //      if (user) {
+    //    	window.location.href ="notes.html";
+      //  } 
+        //else 
+        //{
+        //}
+    //});
+//}
+
+function logOut()
+{
+	firebase.auth().signOut().then(function()
+	{
+		window.location.href ="index.html";
+	})
+
+}
+
+function signInGoogle()
+{
+	var provider = new firebase.auth.GoogleAuthProvider();
+
+	firebase.auth().signInWithPopup(provider).then(function(result) {
+  var token = result.credential.accessToken;
+  var user = result.user;
+}).catch(function(error) {
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  var email = error.email;
+  var credential = error.credential;
+  // ...
+  console.log(error);
+});
+}
+
+
+
+
+function createNote2()
+{
+	name = document.getElementById('txtNoteName').value;
+  	description = document.getElementById('txtNoteDescription').value;
+  	var database = firebase.database();
+  	var userId = firebase.auth().currentUser.uid;
+  	database.ref('users/' + userId + '/'  + 'notes').push(
+  	{
+  		noteName : name,
+  		noteDescription : description
+  	});
+
+  	console.log(name);
+  	console.log(description);
+  	$('#modal .close').click()
+  	  	  window.location.reload(); 
+
+  
+
+}
+
+function drawNotes()
+{
+	var userId = firebase.auth().currentUser.uid;
+	var key = '/users/' + userId + '/' + 'notes';
+	firebase.database().ref('/users/' + userId + '/' + 'notes/').once('value').then(function(snapshot)
+	{
+	const currentUserNotes = snapshot.val();
+			for (var key in currentUserNotes) 
+		{
+	    if (currentUserNotes.hasOwnProperty(key)) 
+	   	 	{
+	   	 		var currentNote = currentUserNotes[key];
+	   	 		console.log(1);
+			  var name = currentNote.noteName;
+			  console.log(name);
+			  var description = currentNote.noteDescription
+
+
+			var html = " <tr> <td><span id='Name" + "'>" + name + "</span></td> <td> <span id='Description" + userId + "'>" + description + "</span></td></tr>"
+					$("#notas").append(html);
+			}
+		}
+
+	});
+}
+
+
+
+
+
+
+//function drawNotes()
+//{
+
+//	var userId = firebase.auth().currentUser.uid;
+//for userId in firebase.database().ref('/users/' + userId + '/' + 'notes/').once('value').then(function(snapshot) {
+ 
+
+  
+//});
+//}
